67332ce3d7e31c6a76ab447339a804fca8d3f545
Sandu Victor <vyctor661@gmail.com>
2024-01-20 14:36:41 +0200

    removed cors
    cors was added as a "fix" for an error in the api, but cors wasnt the problem

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,6 @@ import { logger } from "./src/middleware/logger.js";
 import { errorHandler } from "./src/middleware/errorHandler.js";
 import { DbInit } from "./src/db/dbInit.js";
 import { userController } from "./src/controllers/userController.js";
-import { allowCrossDomain } from "./src/middleware/cors.js";
 
 // Database
 DbInit();
@@ -15,7 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(allowCrossDomain);
 app.use(logger);
 app.use(errorHandler);
 
