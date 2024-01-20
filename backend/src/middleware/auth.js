160119fabd345e8db8ef19f0a34d8868b89ee88e
Sandu Victor <vyctor661@gmail.com>
2024-01-20 17:19:30 +0200

    authorization middleware

---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,42 @@
+import jwt from "jsonwebtoken";
+import { JWT_KEY } from "../config/const.js";
+
+export const authorize = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json("Authorization header not provided");
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json("No token provided");
+    }
+
+    jwt.verify(token, JWT_KEY, (err, decodedToken) => {
+        if (err) {
+            return res.status(401).json("Invalid token");
+        }
+        req.decodedToken = decodedToken;
+        next();
+    });
+};
+
+export const authorizeStudent = (req, res, next) => {
+    authorize(req, res, () => {
+        if (req.decodedToken.type === "Student") {
+            next();
+        } else {
+            return res.status(403).json("Forbidden");
+        }
+    });
+};
+
+export const authorizeProfessor = (req, res, next) => {
+    authorize(req, res, () => {
+        if (req.decodedToken.type === "Professor") {
+            next();
+        } else {
+            return res.status(403).json("Forbidden");
+        }
+    });
+};
