2c5d89504b9d5db845ba31218ccd3bae6fcc8f0e
Sandu Victor <vyctor661@gmail.com>
2024-01-21 16:43:40 +0200

    feedback controller

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import { errorHandler } from "./src/middleware/errorHandler.js";
 import { DbInit } from "./src/db/dbInit.js";
 import { userController } from "./src/controllers/userController.js";
 import { activityController } from "./src/controllers/activityController.js";
+import { feedbackController } from "./src/controllers/feedbackController.js";
 
 // Database
 DbInit();
@@ -20,6 +21,7 @@ app.use(errorHandler);
 
 app.use("/api", userController);
 app.use("/api", activityController);
+app.use("/api", feedbackController);
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running at http://localhost:${PORT}`);
---
+++ backend/src/controllers/feedbackController.js
@@ -0,0 +1,49 @@
+import express from "express";
+import {
+    authorize,
+    authorizeTeacher,
+    authorizeStudent,
+} from "../middleware/auth.js";
+import { getActivityById } from "../models/activity.js";
+import {
+    createFeedback,
+    getFeedbackByActivityId,
+    getFeedbackCountByActvityId,
+} from "../models/feedback.js";
+
+export const feedbackController = express.Router();
+
+feedbackController
+    .route("/createFeedback")
+    .post(authorizeStudent, async (req, res) => {
+        const { activityId, accessCode, type } = req.body;
+
+        if (!activityId || !accessCode || !type)
+            return res.status(400).json("Bad request");
+
+        if (!["smiley", "frowny", "surprised", "confused"].includes(type))
+            return res.status(400).json("Bad request");
+
+        const activity = await getActivityById(activityId);
+        if (!activity) return res.status(400).json("Bad request");
+
+        if (activity.accessCode !== accessCode)
+            return res.status(400).json("Bad request");
+
+        const feedback = await createFeedback(activityId, type);
+
+        return res.status(200).json(feedback);
+    });
+
+feedbackController
+    .route("/getFeedback")
+    .get(authorizeTeacher, async (req, res) => {
+        const { activityId } = req.body;
+
+        if (!activityId) return res.status(400).json("Bad request");
+
+        return res.status(200).json({
+            activities: await getFeedbackByActivityId(activityId),
+            counts: await getFeedbackCountByActvityId(activityId),
+        });
+    });
